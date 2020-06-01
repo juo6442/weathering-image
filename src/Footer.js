550ace354f95c9f1322af46f3cd0b56fb5eaffc9
juo <juo6442@gmail.com>
2020-06-01 16:04:03 +0900

    Remove addresses from footer

--- src/Footer.js
+++ src/Footer.js
@@ -5,10 +5,6 @@ function Footer(props) {
   return (
     <footer>
       <hr />
-      <address>
-        <p>Twitter: <a href="https://twitter.com/juo6442">@juo6442</a></p>
-        <p>E-mail: <a href="mailto:juo6442@gmail.com">juo6442@gmail.com</a></p>
-      </address>
       <p>Github: <a href="https://github.com/juo6442/weathering-image">https://github.com/juo6442/weathering-image</a></p>
     </footer>
   );
