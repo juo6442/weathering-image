5d9b4ab8c7f72bd3cd7f5d5e779b1cabd4ad5820
juo <juo6442@gmail.com>
2020-06-28 14:22:27 +0900

    Implement degrading

--- src/ResultImage.js
+++ src/ResultImage.js
@@ -7,26 +7,58 @@ class ResultImage extends React.Component {
     this.props.onQualityChange(degrade);
   }
 
-  getQualityFromDegrade(degrade) {
-    return 1 - (degrade / 100);
+  getScaleFromDegrade(degrade) {
+    return (100 - degrade) / 100 * 0.5 + 0.5;
   }
 
-  getDegradedImage(source) {
+  getQualityFromDegrade(degrade) {
+    return (100 - degrade) / 100;
+  }
+
+  getResizedImage(image, scale) {
+    return new Promise(function(resolve, reject) {
+      const canvas = document.createElement('canvas');
+      canvas.width = image.width * scale;
+      canvas.height = image.height * scale;
+
+      const ctx = canvas.getContext('2d');
+      ctx.fillStyle = "white";
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+      ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+
+      const resizedImage = new Image();
+      resizedImage.onload = () => {
+        resolve(resizedImage);
+      };
+      resizedImage.src = canvas.toDataURL('image/jpeg', 0);
+    });
+  }
+
+  async getDegradedImage(source) {
     if (!source) return '';
 
     const canvas = document.createElement('canvas');
     canvas.width = source.width;
     canvas.height = source.height;
 
-    let ctx = canvas.getContext('2d');
-    ctx.fillStyle = "white";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.drawImage(source, 0, 0);
+    const ctx = canvas.getContext('2d');
+    ctx.drawImage(await this.getResizedImage(source, this.getScaleFromDegrade(this.props.degrade)), 0, 0, canvas.width, canvas.height);
 
-    return canvas.toDataURL(
+    ctx.globalCompositeOperation = "overlay";
+    ctx.fillStyle = "black";
+    ctx.globalAlpha = (1 - this.getQualityFromDegrade(this.props.degrade)) * 0.7;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    ctx.globalCompositeOperation = "color-burn";
+    ctx.fillStyle = "rgb(234, 225, 176)";
+    ctx.globalAlpha = (1 - this.getQualityFromDegrade(this.props.degrade));
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    const result = canvas.toDataURL(
       'image/jpeg',
       this.getQualityFromDegrade(this.props.degrade)
     );
+    document.getElementById('resultImage').src = result;
   }
 
   render() {
