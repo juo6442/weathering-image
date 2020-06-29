00d5e1ad73432345d8f7a907937abc8737be13d2
juo <juo6442@gmail.com>
2020-06-29 21:15:19 +0900

    Remove SourceImage

--- src/EditorSection.js
+++ src/EditorSection.js
@@ -1,6 +1,4 @@
 import React from "react";
-import SourceImage from "./SourceImage";
-import ResultImage from "./ResultImage";
 import { getDegradedImage } from "./ImageDegrader";
 import "./style.css";
 
@@ -38,17 +36,40 @@ class EditorSection extends React.Component {
       });
   }
 
+  loadSelectedImage() {
+    const file = document.getElementById("imageSelector").files[0];
+
+    const reader = new FileReader();
+    reader.onload = (e) => this.handleImageLoad(e.target.result);
+    reader.readAsDataURL(file);
+  }
+
+  showImageSelector() {
+    const selector = document.getElementById("imageSelector");
+    selector.click();
+  }
+
   render() {
     return (
-      <section>
-        <SourceImage
-          imageData={this.state.imageData}
-          onImageLoad={(data) => this.handleImageLoad(data)}
+      <section className="imageEditor">
+        <div className="buttonContainer">
+          <input type="file" id="imageSelector" accept="image/*"
+            onChange={() => this.loadSelectedImage()}
+          />
+          <input type="button" value="이미지 불러오기"
+            onClick={() => this.showImageSelector()}
+          />
+          <input type="button" value="이미지 저장" />
+        </div>
+
+        <img className="image" alt="불러온 이미지"
+          src={this.state.degradedImageData}
         />
-        <ResultImage
-          degrade={this.state.degrade}
-          degradedImage={this.state.degradedImageData}
-          onQualityChanged={(event) => this.handleQualityChange(event)}
+
+        <input type="range"
+          min="0" max="100" step="5"
+          value={this.state.degrade}
+          onChange={(event) => this.handleQualityChange(event)}
         />
       </section>
     );
